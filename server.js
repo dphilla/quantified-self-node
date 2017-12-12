ed83fb5ac930d7aafe92a2a1f2fcacf2bb3c0ab0
Daniel Phillips <danieledward.phillips@gmail.com>
2017-12-12 08:38:34 -0700

    Add working response from landing and foods index

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-var express = require('express')
-var app = express()
+const environment = process.env.NODE_ENV || 'development';    // if something else isn't setting ENV, use development
+const configuration = require('./knexfile')[environment];    // require environment's settings from knexfile
+const database = require('knex')(configuration);              // connect to DB via knex using env's settings
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //set the port to an ENV variable OR localhost:3000
 app.set('port', process.env.PORT || 3000)
 
 app.get('/', function(request, response) {
   //landing page with html for documentation
+  response.send("This is a test and will be replaced by the api docs!")
 })
 
 
 //Food REST routes
 app.get('/api/v1/foods', function(request, response) {
   //all foods
-  var id = request.params.id
+  //var id = request.params.id
 
-  database.raw(`SELECT * FROM secrets WHERE id = ?`, [id])
+  database.raw(`SELECT * FROM foods`)
     .then(function(data) {
       if (!data.rows[0]) {
         response.sendStatus(404)
       } else {
-        response.json(data.rows[0])
+        response.json(data.rows)
       }
     })
 
@@ -69,4 +76,15 @@ app.post('/api/v1/meals/:meal_id/foods/:id', function(request, response) {
 app.delete('/api/v1/meals/:meal_id/foods/:id', function(request, response) {
   //delete food for meal
 })
+
+
+if (!module.parent) {
+  app.listen(app.get('port'), function() {
+    console.log(`quantified self is running on ${app.get('port')}.`)
+  })
+}
+
+
+
+module.exports = app
 
