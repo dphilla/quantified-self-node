144b8c54a3b49763b7ed87727d7cdb8c4096e4f1
Daniel Phillips <danieledward.phillips@gmail.com>
2017-12-14 16:37:24 -0700

    change seed file

--- knexfile.js
+++ knexfile.js
@@ -34,7 +34,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/production'
+      directory: './seeds/'
     },
     useNullAsDefault: true
   }
