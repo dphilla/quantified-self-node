f01cfc0d4ead0f6b320a8220c42bf32cf37a6734
Daniel Phillips <danieledward.phillips@gmail.com>
2017-12-13 18:03:48 -0700

    complete Delete food functionality

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const environment = process.env.NODE_ENV || 'development';    // if something else isn't setting ENV, use development
 const configuration = require('./knexfile')[environment];    // require environment's settings from knexfile
 const database = require('knex')(configuration);              // connect to DB via knex using env's settings
@@ -26,14 +27,9 @@ app.post('/api/v1/foods', food.postFood)
 
 app.get('/api/v1/foods/:id', food.getSingleFood)
 
-app.patch('/api/v1/foods/:id', food.updateFood)
+app.patch('/api/v1/foods/:id', food.updateFood) // confirm this is working and refactor model method
 
-
-
-
-app.delete('/api/v1/foods/:id', function(request, response) {
-  //delete food
-})
+app.delete('/api/v1/foods/:id', food.deleteFood)
 
 
 //Meals
