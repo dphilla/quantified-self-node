8120d328cd73e7a7c7c54134c2a39f0a4251607a
samuelssnider <sam_redins@yahoo.com>
2018-01-25 06:50:19 +0900

    taking some debuggers out

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const environment = process.env.NODE_ENV || 'development';    // if something else isn't setting ENV, use development
 const configuration = require('./knexfile')[environment];    // require environment's settings from knexfile
 const database = require('knex')(configuration);              // connect to DB via knex using env's settings
