53e05565f357ee356cda33b895980e1aea956562
samuelssnider <sam_redins@yahoo.com>
2018-01-26 13:28:06 +0900

    random meal and food id generated for meal foods, seed file working

--- db/seeds/data/meal_foods.js
+++ db/seeds/data/meal_foods.js
@@ -1,9 +1,16 @@
 const foods = require('../data/foods')
 const meals = require('../data/meals')
-
+const MEAL_FOODS = []
 function generateRandomMealFood() {
   return random_meal_food = {
-    meal_id:
+    meal_id: (Math.floor(Math.random() * 4)) + 1,
+    food_id: (Math.floor(Math.random() * 12)) + 1,
     created_at: new Date
   }
 }
+
+for (var i = 0; i < 12; i ++) {
+  MEAL_FOODS.push(generateRandomMealFood(i))
+}
+
+module.exports = { MEAL_FOODS }
